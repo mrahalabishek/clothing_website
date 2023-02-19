b046bff3a10a61a491f437dd959820def8935ca6
mrahalabishek <mrahalabishek@gmail.com>
2023-02-19 10:19:06 +0530

    create one page website

--- header.php
+++ header.php
@@ -3,19 +3,10 @@
         <a href="index.php"><img src="images/logo.jpg" alt="Chic Clothing logo" /></a>
         </div><!--logo ends-->
 		<div class="navigation">
-        
+        <p>
         <?php
-            $cat_sql="SELECT * FROM category";
-            $cat_query=mysqli_query($dbconnect, $cat_sql);
-            $cat_rs=mysqli_fetch_assoc($cat_query);
-        ?>
-		<p><?php
-        do {    ?>
-            <a href="category.php?categoryID=<?php echo $cat_rs['categoryID']; ?>"><?php echo $cat_rs['name']; ?></a>
-
-            <?php
-        }while($cat_rs=mysqli_fetch_assoc($cat_query));
-        ?>
+            include("categoryList.php");
+           ?>
         <a href="admin.php">Admin</a>
         </p>
       </div><!--navigation ends-->
---
+++ home.php
@@ -0,0 +1,2 @@
+<h1>Welcome to Chic Clothing</h1>
+<p>This is our home page</p>
